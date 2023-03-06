f937263ff05528c04dafcef0b67b140524bded95
Icarus <icarocampos@proton.me>
2023-03-06 14:01:22 -0300

    Project setup

---
+++ app/src/main/java/com/ics/ifoodclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ics.ifoodclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
