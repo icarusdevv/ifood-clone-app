3f8fbd840612a2c56e46f89913892524ce680786
Icarus <icarocampos@proton.me>
2023-03-06 21:54:11 -0300

    Adding bottom navigation

--- app/src/main/java/com/ics/ifoodclone/presentation/MainActivity.kt
+++ app/src/main/java/com/ics/ifoodclone/presentation/MainActivity.kt
@@ -2,13 +2,27 @@ package com.ics.ifoodclone.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.ics.ifoodclone.R
 import com.ics.ifoodclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding  = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
+
     }
 }
